c39fa5ef7ef5bfa81aeb557259ebfc967eb27315
Andreas Lojdahl <andreas.lojdahl@gmail.com>
2020-09-04 10:45:01 +0200

    added scrubber class small changes in ica harvester

--- backend/classes/IcaHarvester.js
+++ backend/classes/IcaHarvester.js
@@ -17,28 +17,29 @@ module.exports = class IcaHarvester {
 
     let products = await this.getProductIds(categoryURL);
     let productIds = products.items.map(x=> x.id);
-    let ids = productIds.filter(function(x){
-      return x.length === 13;
-    })
+    //***** Suuposed to remove the commercial ad */
+    // let ids = productIds.filter(function(x){
+    //   return x.length === 13;
+    // })
 
-    while(ids.length > 0 ){
+    while(productIds.length > 0 ){
       //Supposed to fetch 99 items a time
-      idsToFetch.push(ids.splice(0, 10));
+      idsToFetch.push(productIds.splice(0, 99));
     }
 
     for(let i=0; i< idsToFetch.length; i++){
 
-      console.log(idsToFetch[i].toString());
       let raw = await fetch(
         "https://handla.ica.se/api/content/v1/collection/customer-type/B2C/store/maxi-ica-stormarknad-malmo-id_02748/products-data?skus=" +
           idsToFetch[i].toString()
       );
-      fetchedItems.push(await raw.json());
+      let res = await raw.json();
+      fetchedItems = [...fetchedItems,...res ];
     }
+
     return fetchedItems;
   }
   
-
   static async getCategories() {
     let raw = await fetch(
       "https://handla.ica.se/api/product-info/v1/store/11981/category/catalog80002"
