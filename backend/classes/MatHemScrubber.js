794c2a56ea8cf84934e9276168678955610012fa
Andreas Lojdahl <andreas.lojdahl@gmail.com>
2020-09-09 10:56:07 +0200

    added restcontroller

--- backend/classes/MatHemScrubber.js
+++ backend/classes/MatHemScrubber.js
@@ -8,11 +8,11 @@ module.exports = class MatHemScrubber extends Scrubber {
     brand: (x) => x.brand.name,
     imageUrl: (x) => x.images.MEDIUM,
     price: (x) => x.price,
-    compareProductVolumeUnit: (x) => x.unit,
+    productVolumeUnit: (x) => x.unit,
     productVolume: (x) => x.quantity,
     comparePrice: (x) => x.comparisonPrice,
     compareUnit: (x) => x.comparisonUnit,
-    inStock: (x) => (x.availability === "AVAILABLE" ? true : false),
+    // inStock: (x) => (x.availability === "AVAILABLE" ? true : false),
     eco: (x) => x.badges.length > 0 ? x.badges.filter((x) => x.name === "Ekologisk") ? true : false : false,
     Swedish: (x) => (x.origin || {}).name === "Sverige",
     originCountry: (x) => (x.origin || {}).name || "unknown"
