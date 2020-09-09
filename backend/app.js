23cadef655c1bac76b68fac1c59cf3b7379e21f9
Work <vikk@live.se>
2020-09-09 09:35:46 +0200

    Added database file, connected it with project -Viktor

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,30 @@
 const Harvester = require('./classes/Harvester')
+const sqlite3 = require('sqlite3')
+const express = require('express')
+const util = require('util')
+
+const app = express()
+
+let db = new sqlite3.Database('./MatJakt, SQLite.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the database.');
+});
+
+db.serialize(() => {
+  db.each(`SELECT storeId as id,
+                  name as name
+           FROM stores`, (err, row) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(row.id + "\t" + row.name);
+  });
+});
+
+
+
 
 let categoryURL = "Kott-chark-och-fagel/Kott";
 
@@ -7,7 +33,23 @@ let categoryURLIca = "frukt---gront-id_627";
 
 let categoryURLMatHem = "frukt-o-gront";
 
-Harvester.getMatHemProducts(categoryURLMatHem)
+//db.all(   'SELECT * FROM stores',   (err, stores) => {     for (let store of stores) {       console.log(store.json);    }   });
+
+app.get('/rest/hello', (req, res) => {
+    res.json({
+        message: 'Hello World'})})
+
+app.get('/rest/stores', async (req, res) => {
+    let stores = await db.all('SELECT * FROM stores')
+
+    res.json({stores })
+})
+
+app.listen(3000, () => {
+console.log('Listening on 3000')
+})
+
+//Harvester.getMatHemProducts(categoryURLMatHem)
 
   // Harvester.getWillysProducts(categoryURL);
   //Harvester.getIcaProducts(categoryURLIca);
