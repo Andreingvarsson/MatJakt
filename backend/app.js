ece51d5d889dd89619c5fcd82c7f292a19fdd02f
Work <vikk@live.se>
2020-09-09 11:31:55 +0200

    Added better-sqlite, removed redundant dependencies -Viktor

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,15 @@
 const Harvester = require('./classes/Harvester')
-const sqlite3 = require('sqlite3')
-const express = require('express')
-const util = require('util')
+const DbHandler = require('./classes/DBHandler');
 
-const app = express()
+const db = new DbHandler('./database/MatJaktDatabase.db');
 
-let db = new sqlite3.Database('./MatJakt, SQLite.db', (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the database.');
-});
+const all = db.all(
 
-db.serialize(() => {
-  db.each(`SELECT storeId as id,
-                  name as name
-           FROM stores`, (err, row) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log(row.id + "\t" + row.name);
-  });
-});
+  'SELECT * FROM stores',
 
+);
+
+console.log('All stores', all);
 
 
 
@@ -32,22 +19,6 @@ let categoryURL = "Kott-chark-och-fagel/Kott";
 let categoryURLIca = "frukt---gront-id_627";
 
 let categoryURLMatHem = "frukt-o-gront";
-
-//db.all(   'SELECT * FROM stores',   (err, stores) => {     for (let store of stores) {       console.log(store.json);    }   });
-
-app.get('/rest/hello', (req, res) => {
-    res.json({
-        message: 'Hello World'})})
-
-app.get('/rest/stores', async (req, res) => {
-    let stores = await db.all('SELECT * FROM stores')
-
-    res.json({stores })
-})
-
-app.listen(3000, () => {
-console.log('Listening on 3000')
-})
 
 //Harvester.getMatHemProducts(categoryURLMatHem)
 
