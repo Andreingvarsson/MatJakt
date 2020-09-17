62933a4a32a1157708af61c40a4dce7d118e0233
André <andreingvarsson@hotmail.com>
2020-09-17 14:38:45 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
-  //Switch,
-  //Route,
+  Switch,
+  Route,
   //Link
 } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Router>
-    <div className="App">
-    <h1>Hejsan Kewla dudes</h1>
+    <Router>  
+    <div className="App"> 
+    {/* contextProvider */}
+    <Switch>
+      <Route exact path="/" component={home}/>
+    </Switch>
+    {/* contextProvider */}
     </div>
     </Router>
     </>
