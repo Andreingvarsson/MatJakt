c713e198eb230f5e47cef4c6cbcd35ee91145db0
Work <vikk@live.se>
2020-09-17 11:38:39 +0200

    Added function to fetch from API to frontend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const getData = async () => {
+
+  let data = await fetch('/api/products') //Gets all products
+
+  data = await data.json()
+
+  console.log(data)
+
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +19,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          <button onClick={getData}>
+            click me
+          </button>
         </p>
         <a
           className="App-link"
