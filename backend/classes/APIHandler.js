28dd97f3897539982c2267b1472a37df4dda2ff7
Work <vikk@live.se>
2020-09-28 14:41:25 +0200

    Added a APIHandler-class and moved relevant code to it -Viktor

---
+++ backend/classes/APIHandler.js
@@ -0,0 +1,29 @@
+const DbHandler = require("./DBHandler");
+const db = new DbHandler("./database/MatJaktDatabase.db");
+
+module.exports = class APIHandler {
+
+  categoryProducts(req, res) {
+    const limit = req.query.limit ? ` LIMIT ` + req.query.limit : "";
+    const page = req.query.page ? ` OFFSET ` + req.query.page * 10 : "";
+    console.log(req.query.limit + " req limit" + req.query.page);
+    let anyProducts = db.all(
+      "SELECT * FROM products WHERE categoryId = " +
+      req.params.categoryId +
+      " ORDER BY price ASC " +
+      limit +
+      page
+    );
+    res.json(anyProducts);
+  }
+
+  searchedProducts(req, res) {
+    const limit = req.query.limit ? ` LIMIT ` + req.query.limit : "";
+    const page = req.query.page ? ` OFFSET ` + req.query.page * 10 : "";
+    console.log(req.query.limit + " req limit" + req.query.page + 'INNNNNEEE I BACK');
+    let anyProducts = db.all(
+      "SELECT * FROM products WHERE name LIKE " + "'" + "%" + req.params.searchWord + "%" + "'" + " ORDER BY price ASC " + limit + page
+    );
+    res.json(anyProducts);
+  }
+}
