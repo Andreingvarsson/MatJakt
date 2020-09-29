1c551a619796211ac566a7d8c3a62e88f9bbe8c3
André <andreingvarsson@hotmail.com>
2020-09-29 10:24:02 +0200

    changed divs, and h3s, to Card

--- src/Pages/CheapestResult.js
+++ src/Pages/CheapestResult.js
@@ -12,33 +12,33 @@ const CheapestResult = (props) => {
   return (
     <>
     <Card>
-      <div className="page">
-        <h1 className="">Din inköpslista</h1>
-        <h3 className="">blir billigast hos: </h3>
+      <CardBody className="page">
+        <CardTitle className="">Din inköpslista</CardTitle>
+        <CardText className="">blir billigast hos: </CardText>
         <div classname="first-seg">
-          <img
+          <CardImg
             className="responsive"
             src="https://lh3.googleusercontent.com/proxy/rLv9GbeKWzlSmi8yWR1bROlxf2P1fJWBcqrZ3TfWfWR9PRiMtiar1f7VeMcuiF6lnmZaMy0EhZYY4XCSAJYM2UusG5GiYYXIsInGV-Wv_o5MMcOJmMmvUZuUXuN_prEyJ_j-KHN5QcXmpcgfXceNQAwU1L91"
-          ></img>
-          <h3>med en totalsumma på</h3>
-          <h3> 387 kr </h3>
+          ></CardImg>
+          <CardText>med en totalsumma på</CardText>
+          <CardText> 387 kr </CardText>
         </div>
         <div className="line"></div>
         <div className="second-seg">
-          <img
+          <CardImg
             className="responsive"
             src="https://static.hitta.se/static/products/images/InfoTextDeluxe/icaboras3_Il.png"
-          ></img>
-          <h5> 500 kr </h5>
+          ></CardImg>
+          <CardText>500 kr</CardText>
         </div>
         <div className="second-seg">
-          <img
+          <CardImg
             className="responsive"
             src="https://images.saasm2m.com/m2mstatic/images/butik/5747.jpg"
-          ></img>
-          <h5> 400 kr </h5>
+          ></CardImg>
+          <CardText> 400 kr </CardText>
         </div>
-      </div>
+      </CardBody>
       </Card>
     </>
   );
