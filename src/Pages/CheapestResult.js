ae8a164d9022af60b85974ba56c788f599376561
André <andreingvarsson@hotmail.com>
2020-10-01 10:17:32 +0200

    changed positioning, css, for the page to fit wireframe.

--- src/Pages/CheapestResult.js
+++ src/Pages/CheapestResult.js
@@ -11,35 +11,37 @@ const CheapestResult = (props) => {
 
   return (
     <>
-    <Card>
+    
+    <Card className="container card-border top-margin background">
       <CardBody className="page">
-        <CardTitle className="">Din inköpslista</CardTitle>
-        <CardText className="">blir billigast hos: </CardText>
+        <CardTitle className="title">Din inköpslista</CardTitle>
+        <CardText className="text">blir billigast hos: </CardText>
         <div classname="first-seg">
           <CardImg
-            className="responsive"
-            src="https://lh3.googleusercontent.com/proxy/rLv9GbeKWzlSmi8yWR1bROlxf2P1fJWBcqrZ3TfWfWR9PRiMtiar1f7VeMcuiF6lnmZaMy0EhZYY4XCSAJYM2UusG5GiYYXIsInGV-Wv_o5MMcOJmMmvUZuUXuN_prEyJ_j-KHN5QcXmpcgfXceNQAwU1L91"
+            className="col-8"
+            src="https://static.hitta.se/static/products/images/InfoTextDeluxe/icaboras3_Il.png"
           ></CardImg>
-          <CardText>med en totalsumma på</CardText>
-          <CardText> 387 kr </CardText>
+          <CardText className="text">med en totalsumma på</CardText>
+          <CardText className="text bold"> 387 kr </CardText>
         </div>
         <div className="line"></div>
-        <div className="second-seg">
+        <div className="second-seg col-6">
           <CardImg
             className="responsive"
             src="https://static.hitta.se/static/products/images/InfoTextDeluxe/icaboras3_Il.png"
           ></CardImg>
-          <CardText>500 kr</CardText>
+          <CardText className="col-10 middle sec-seg-text">500 kr</CardText>
         </div>
-        <div className="second-seg">
+        <div className="second-seg col-6">
           <CardImg
             className="responsive"
-            src="https://images.saasm2m.com/m2mstatic/images/butik/5747.jpg"
+            src="https://static.hitta.se/static/products/images/InfoTextDeluxe/icaboras3_Il.png"
           ></CardImg>
-          <CardText> 400 kr </CardText>
+          <CardText className="col-10 middle sec-seg-text"> 400 kr </CardText>
         </div>
       </CardBody>
       </Card>
+   
     </>
   );
 };
