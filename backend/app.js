b71417b88296da8f5191de4af18e42619361fe9c
Andreas LÖjdahl <andreas.lojdahl@gmail.com>
2020-10-08 12:29:20 +0200

    removed comments and updated queries Andreas, andre, viktor

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,6 @@ setInterval(() => {
 
 //Checks the current date and compares it to the dates for each harvest/fetch for the stores
 //If it has been 24 hours (86400 seconds) since the last fetch it will run the harvest functions
-//TODO, clean up console/make it more readable
 async function checkForHarvest() {
   console.log('\n' + Date().toString('yyyy-MM-d-h-mm-ss') + '\n' + "Checking if harvest is needed" + '\n')
   var currentDate = Math.round((new Date()).getTime() / 1000);
@@ -47,73 +46,6 @@ app.get("/api/categories", api.categories);
 app.get("/api/catProducts/:categoryId", api.categoryProducts);
 
 app.get("/api/searchProducts/:searchWord", api.searchedProducts);
-
-
-
-
-// //************ CATEGORY PRODUCTS*********************** */
-// app.get("/api/catProducts/:categoryId", (req, res) => {
-//   const limit = req.query.limit ? ` LIMIT ` + req.query.limit : "";
-//   const page = req.query.page ? ` OFFSET ` + req.query.page * 10 : "";
-//   const eco = req.query.eco;
-//   const swe = req.query.swe;
-//   console.log(req.query.limit + " req limit" + req.query.page);
-//   if(eco === 'true' && swe === 'true'){
-//     let products = db.all(
-//       "SELECT * FROM products WHERE categoryId = " +req.params.categoryId +" AND products.eco = 1 AND products.Swedish = 1 ORDER BY price ASC " + limit + page
-//       )
-//       return res.json(products)
-//   }else if(eco === 'true'){
-//     let products = db.all(
-//     "SELECT * FROM products WHERE categoryId = " +req.params.categoryId +" AND products.eco = 1 ORDER BY price ASC " + limit + page
-//     )
-//     return res.json(products)
-//   }else if(swe === 'true'){
-//     let products = db.all(
-//     "SELECT * FROM products WHERE categoryId = " +req.params.categoryId +" AND products.Swedish = 1 ORDER BY price ASC " + limit + page
-//     )
-//     return res.json(products)
-//   }
-//   let anyProducts = db.all(
-//     "SELECT * FROM products WHERE categoryId = " +req.params.categoryId +" ORDER BY price ASC " + limit + page
-//   );
-//   return res.json(anyProducts);
-// });
-
-
-//  /************************SEARCH ECO/SWEDISH/ALL ATTEMPT*************************************** */
-
-//  app.get("/api/searchProducts/:searchWord", (req, res) => {
-//   const limit = req.query.limit ? ` LIMIT ` + req.query.limit : "";
-//   const page = req.query.page ? ` OFFSET ` + req.query.page * 10 : "";
-//   const eco = req.query.eco;
-//   const swe = req.query.swe;
-//   console.log(req.query.limit + " req limit" + req.query.page + 'INNNNNEEE I BACK');
-//   console.log(eco);
-//   if(eco === 'true' && swe === 'true'){
-//     let products = db.all(
-//       "SELECT categories.name, products.* FROM products, categories WHERE products.categoryId = categories.categoryId AND products.name LIKE "+"'"+"%"+req.params.searchWord+"%"+"'"+" AND products.eco = 1 AND products.Swedish = 1 ORDER BY  INSTR("+"'"+"Fryst, Skafferi Djur"+"'"+", categories.name), length(products.name) ASC" + limit + page
-//     )
-//     return res.json(products)
-//   }else if(eco === 'true'){
-//     console.log('ECO TRUE')
-//     let ecoProducts = db.all(
-//     "SELECT categories.name, products.* FROM products, categories WHERE products.categoryId = categories.categoryId AND products.name LIKE "+"'"+"%"+req.params.searchWord+"%"+"'"+" AND products.eco = 1 ORDER BY  INSTR("+"'"+"Fryst, Skafferi Djur"+"'"+", categories.name), length(products.name) ASC" + limit + page
-//     )
-//     return res.json(ecoProducts)
-
-//   }else if(swe === 'true'){
-//     let swedishProducts = db.all(
-//       "SELECT categories.name, products.* FROM products, categories WHERE products.categoryId = categories.categoryId AND products.name LIKE "+"'"+"%"+req.params.searchWord+"%"+"'"+" AND products.Swedish = 1 ORDER BY  INSTR("+"'"+"Fryst, Skafferi Djur"+"'"+", categories.name), length(products.name) ASC" + limit + page
-//       )
-//      return res.json(swedishProducts);
-//   }
-//   let anyProducts = db.all(
-//     "SELECT categories.name,  products.* FROM products, categories WHERE products.categoryId = categories.categoryId AND products.name LIKE "+"'"+"%"+req.params.searchWord+"%"+"'"+" ORDER BY  INSTR("+"'"+"Fryst, Skafferi Djur"+"'"+", categories.name), length(products.name) ASC" + limit + page
-//   );
-//   return res.json(anyProducts);
-// });
-
 
 app.use(express.static(__dirname + "src")); 
 
