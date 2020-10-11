26e5fc203b2731dbd1f3a2b4d57757ce8e659f43
Andreas LÖjdahl <andreas.lojdahl@gmail.com>
2020-10-11 14:57:27 +0200

    fixed some css in header links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -18,13 +18,13 @@ const Header = (props) => {
 <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top bg-dark">
   <div className="container ">
 
-    <h1 className="monospace-font font-white ">MatJakt</h1>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#links" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <h3 className="monospace-font font-white col-xs-4 col-sm-6 col-md-6 col-l-6 col-xl-6 ">MatJakt</h3>
+    {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#links" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse " id="links">
-      <Link className="nav-link btn btn-light monospace-font" to="/hem">Sök varor</Link>
-      <Link className="nav-link btn btn-light monospace-font ml-3" to="/inkopslista">Inköpslista<span></span></Link>
+    </button> */}
+    <div className="collapse navbar-collapse d-flex justify-content-end col-xs-8 col-sm-6 col-md-6 col-l-6 col-xl-6" id="links">
+      <Link className="nav-link btn btn-light monospace-font col-6" to="/hem">Sök varor</Link>
+      <Link className="nav-link btn btn-light monospace-font ml-3 col-6" to="/inkopslista">Inköpslista<span></span></Link>
    
     </div>
   </div>
